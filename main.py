01c87fd4bbbe4cb247e21f56fb14d6cb41920968
kkatayama <katayama@udel.edu>
2024-03-03 13:38:09 -0500

    config updates

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
-
+"""
+  POST: to create data.
+   GET: to read data.
+   PUT: to update data.
+DELETE: to delete data.
+"""
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI()
+api = FastAPI()
 
 
-@app.get("/")
-def read_root():
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+
+@api.get("/")
+def root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+@api.get("/items/{item_id}")
+def get_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@api.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item.price": item.price, "item_id": item_id}
