6c1e7f791085e8a655e3440d9870dda3f17f0841
kkatayama <kkatayama@udel.edu>
2024-03-10 18:24:22 -0400

    condense auth_api, working on auth routes

--- secure_api/routes/auth.py
+++ secure_api/routes/auth.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 from sqlmodel import Session
-from fastapi import APIRouter, Depends
-
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from secure_api.database.database import get_session
@@ -13,16 +12,20 @@ from secure_api import configs
 auth_router = APIRouter()
 
 
-@auth_router.post("/auth/login", response_model=models.TokenSchema)
-def login(*, form: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_session)):
-    user = auth_api.authenticate_user(db, form.username, form.password)
-    expires = timedelta(minutes=configs.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = auth_api.create_access_token(item=user.email, expires_delta=expires)
-    return {"access_token": access_token, "token_type": "bearer"}
+@auth_router.post("/login", summary="Create access and refresh tokens for user", response_model=models.TokenSchema)
+def login(*, form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_session)):
+    user = db.exec(select(models.User).where(models.User.email == form_data.username)).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid email")
+    if not auth_api.verify_password(form_data.password, user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect password")
 
+    return models.TokenSchema(
+        access_token=auth_api.create_access_token(user.email),
+        refresh_token=auth_api.create_refresh_token(user.email))
 
 @auth_router.get("/auth/me", response_model=models.User)
-def read_auth_me(*, current_user: models.User = Depends(auth_api.get_current_active_user)):
+def read_auth_me(*, current_user: models.User = Depends(auth_api.get_current_user)):
     return current_user
 
 
