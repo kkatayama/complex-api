781e0edfea4abff3ea5e8ecff1f37a953b55f1c0
kkatayama <kkatayama@udel.edu>
2024-03-10 14:53:41 -0400

    cleanup

--- secure_api/routes/auth.py
+++ secure_api/routes/auth.py
@@ -13,15 +13,15 @@ from secure_api import configs
 auth_router = APIRouter()
 
 
-@auth_router.post("/auth/login", response_model=models.Token)
+@auth_router.post("/auth/login", response_model=models.TokenSchema)
 def login(*, form: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_session)):
     user = auth_api.authenticate_user(db, form.username, form.password)
     expires = timedelta(minutes=configs.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = auth_api.create_access_token(data={"sub": user.email}, expires_delta=expires)
+    access_token = auth_api.create_access_token(item=user.email, expires_delta=expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@auth_router.get("/auth/me", response_model=models.UserRead)
+@auth_router.get("/auth/me", response_model=models.User)
 def read_auth_me(*, current_user: models.User = Depends(auth_api.get_current_active_user)):
     return current_user
 
